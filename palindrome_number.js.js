e564dbbea8f56266cd12568827c1318dd87660e8
JaredShakespaere <90650932+JaredShakespaere@users.noreply.github.com>
2022-04-30 09:17:12 -0500

    turned the input number into a string, split the string and trying to put the string into another variable backwards

--- palindrome_number.js.js
+++ palindrome_number.js.js
@@ -1,3 +1,18 @@
 // Given an integer x, return true if x is palindrome integer.
 
-var isPalindrome = function (x) {};
+var isPalindrome = function (x) {
+	let stringX = x.toString();
+	let splitX = stringX.split('');
+
+	for (let i = 0; i < splitX.length; i++) {
+		console.log(splitX);
+
+		splitX.pop();
+	}
+
+	// console.log(popX)
+	// console.log(sliceX)
+	// if(x < 0){
+	//     return false;
+	// } else if()
+};
